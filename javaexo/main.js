362d2717000098af3d7acdaaf7476ed5e8b51096
albanscud <113613752+albanscud@users.noreply.github.com>
2022-10-06 17:13:04 +0200

    Add files via upload

---
+++ javaexo/main.js
@@ -0,0 +1,56 @@
+//exercice 1
+
+
+alert("bonjour tout le monde");
+let start ="bonjour ",   
+    prenom,
+    end = " !",
+    result;
+
+prenom = prompt("quel est votre prenom?");  
+result = start + prenom + end;              
+alert(result);        
+
+//exercice 2
+
+/* let number1, number2;
+number1 = parseInt (prompt("entrez un nombre"));
+number2 = parseInt (prompt("entrez votre deuxieme nombre"));
+alert (number1+number2); */
+
+
+//exercice 3
+
+
+/* let number = prompt("entrez votre age");
+
+    if (number > 0) {
+        alert ("correct")
+    } else 
+        alert("erreur ! veuillez ressaisir votre age") 
+     */
+    
+//exercice 5 (faux)
+
+/* document.write
+for (let i=1;i<=10;i++){
+    document.write(i)
+}
+
+for (let i=1;i<10;i++)
+{
+ document .write(i)
+ for (let j=1;j<=10;j++)
+ {
+    document.write(i*j)
+ }   
+}
+ */
+
+//exercice 6
+
+/* let element = document.getElementById('clickhere');
+
+element.addEventListener('click', function(){
+    element.style.backgroundColor = "#FF00F0";
+    },false); */
